3a0f5b50bb79fef248a6b21e75193746fb444cdd
Heather Nuffer <zzzuzz@gmail.com>
2019-12-19 16:41:32 -0700

    Added content to Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import Heather from '../images/HeatherNuffer.jpg';
 
 const Home = props => {
+    
+    const handleClick = () => {
+        props.history.push('/projects');
+    }
+
     return (
-        <div>
-            <h1>Heather Nuffer</h1>
-            <h2>Full-Stack Web Developer</h2>
+        <div className='home-box'>
+            <div className='left-content'>
+                <div className='text-box'>
+                    <h1>Heather Nuffer</h1>
+                    <h2>Full-Stack Web Developer</h2>
+                </div>
+                <button onClick={handleClick}>Explore My Portfolio</button>
+            </div>
+            <div className='image-container'>
+                <img src={Heather} alt='Heather Nuffer' />
+            </div>
         </div>
     )
 }
