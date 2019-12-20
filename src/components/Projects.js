e1a0847429e9b9094020416b3b8726f1cbfffb5d
Heather Nuffer <zzzuzz@gmail.com>
2019-12-19 17:11:14 -0700

    Added first project's info to data file and project page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import projectData from '../projectData';
 
 const Projects = props => {
+    console.log(projectData);
     return (
         <div className='home-box'>
-            <h3>Portfolio </h3>
-            <p>projects go here</p>
+            <h1>Portfolio</h1>
+            <div>
+                <h3>{projectData[0].title}</h3>
+                <p>{projectData[0].description}</p>
+                <a href={projectData[0].deployed} >{projectData[0].title}</a>
+                <a href={projectData[0].repo}>Repository on GitHub</a>
+            </div>
         </div>
     )
 }
---
+++ src/projectData.js
@@ -0,0 +1,9 @@
+const projectData = [
+    {'title': 'GitHubUser',
+    'repo': 'https://github.com/ethyl2/React-Github-User-Card',
+    'deployed': 'https://github-user.now.sh/',
+    'description': 'This project is built with React and uses axios to access data from GitHub\'s API to populate a page with info about a GitHub user. The GitHub calendar for the user is also displayed. Entering a username reloads the page with information for that user.'
+    }
+]
+
+export default projectData;
