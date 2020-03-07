ff9755b2dbd679ae1ed09c42d136d204f8a1ec13
Heather Nuffer <zzzuzz@gmail.com>
2020-03-06 18:02:09 -0700

    Adds social icons to footer to link to external sites

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,41 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faGithub,
+  faTwitter,
+  faLinkedin
+} from '@fortawesome/free-brands-svg-icons';
+import '@fortawesome/fontawesome-svg-core/styles.css';
 
 const Footer = props => {
   return (
     <footer>
-      <p>© 2020 Heather Nuffer</p>
+      <div>
+        <a
+          href="https://github.com/ethyl2"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon icon={faGithub} />
+        </a>
+        <a
+          href="https://twitter.com/ethyl22000"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon icon={faTwitter} />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/heather-nuffer/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon icon={faLinkedin} />
+        </a>
+      </div>
+      <div>
+        <p>© 2020 Heather Nuffer</p>
+      </div>
     </footer>
   );
 };
