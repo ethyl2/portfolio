38609b3a41578abd20347820c4c6977ad5b4cc57
Heather Nuffer <zzzuzz@gmail.com>
2020-06-09 13:54:12 -0600

    Changes home button to 'explore my projects' to match nav bar

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,26 +1,25 @@
 import React from 'react';
 import Heather from '../images/HeatherNuffer.jpg';
 
-const Home = props => {
-    
-    const handleClick = () => {
-        props.history.push('/projects');
-    }
+const Home = (props) => {
+  const handleClick = () => {
+    props.history.push('/projects');
+  };
 
-    return (
-        <div className='home-box'>
-            <div className='left-content'>
-                <div className='text-box'>
-                    <h1>Heather Nuffer</h1>
-                    <h2>Full-Stack Web Developer</h2>
-                </div>
-                <button onClick={handleClick}>Explore My Portfolio</button>
-            </div>
-            <div className='image-container'>
-                <img src={Heather} alt='Heather Nuffer' />
-            </div>
+  return (
+    <div className="home-box">
+      <div className="left-content">
+        <div className="text-box">
+          <h1>Heather Nuffer</h1>
+          <h2>Full-Stack Web Developer</h2>
         </div>
-    )
-}
+        <button onClick={handleClick}>Explore My Projects</button>
+      </div>
+      <div className="image-container">
+        <img src={Heather} alt="Heather Nuffer" />
+      </div>
+    </div>
+  );
+};
 
 export default Home;
